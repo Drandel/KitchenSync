bcc34c91013e112ed9d13236e9d4e4c27417dc99
Dean <dee.randel@gmail.com>
2026-03-10 10:46:25 -0600

    refactor(auth): extract AuthTextInput component from Login and Signup                                                                                                               Create AuthTextInput, a variant-driven wrapper around AuthTextField                                         that owns icon selection, placeholder, input type, and password toggle   adornments internally. Signup and Login now declare fields with a                                           single variant prop instead of repeating slotProps and icon markup                                          inline for each field.

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,19 +1,13 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
 import Divider from "@mui/material/Divider";
 import Link from "@mui/material/Link";
-import EmailIcon from "@mui/icons-material/Email";
-import LockIcon from "@mui/icons-material/Lock";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
 import GoogleG from "./GoogleG";
+import AuthTextInput from "./AuthTextInput";
 import {
   DARK_GREEN,
-  ICON_BROWN,
   MUTED_GRAY,
   ERROR_RED,
   WARNING_AMBER,
@@ -31,10 +25,12 @@ import {
   warningBoxSx,
   errorBoxSx,
   oauthButtonSx,
-  AuthTextField,
   SubmitButton,
 } from "../styles/authForms";
-import type { AuthFormState, AuthFormAction } from "../reducers/authFormReducer";
+import type {
+  AuthFormState,
+  AuthFormAction,
+} from "../reducers/authFormReducer";
 
 export interface LoginFormProps {
   authFormData: AuthFormState;
@@ -59,7 +55,6 @@ export default function LoginForm({
 }: LoginFormProps) {
   return (
     <>
-      {/* Logo */}
       <Box sx={logoBoxSx}>
         <RestaurantMenuIcon sx={{ fontSize: 28 }} />
         <Typography variant="h6" fontWeight={700} sx={logoTextSx}>
@@ -67,7 +62,6 @@ export default function LoginForm({
         </Typography>
       </Box>
 
-      {/* Title */}
       <Typography variant="h4" fontWeight={700} sx={pageTitleSx}>
         Welcome Back
       </Typography>
@@ -75,57 +69,26 @@ export default function LoginForm({
         Sign in to continue to KitchenSync.
       </Typography>
 
-      {/* Unified card: cork top + cream bottom */}
       <Box sx={outerCardSx}>
-        {/* Cork section: form fields + submit */}
         <Box component="form" onSubmit={onSubmit} noValidate sx={corkSectionSx}>
-          <AuthTextField
-            fullWidth
-            type="email"
-            placeholder="Email"
+          <AuthTextInput
+            variant="email"
             value={authFormData.email}
-            onChange={(e) => dispatch({ type: "emailUpdated", payload: e.target.value })}
-            required
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <EmailIcon sx={{ color: ICON_BROWN }} />
-                  </InputAdornment>
-                ),
-              },
-            }}
+            onChange={(val) => dispatch({ type: "emailUpdated", payload: val })}
           />
 
-          <AuthTextField
-            fullWidth
-            type={authFormData.showPassword ? "text" : "password"}
-            placeholder="Password"
+          <AuthTextInput
+            variant="password"
             value={authFormData.password}
-            onChange={(e) => dispatch({ type: "passwordUpdated", payload: e.target.value })}
-            required
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LockIcon sx={{ color: ICON_BROWN }} />
-                  </InputAdornment>
-                ),
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() => dispatch({ type: "passwordVisibilityToggled" })}
-                      edge="end"
-                      size="small"
-                      sx={{ color: ICON_BROWN }}
-                      aria-label={authFormData.showPassword ? "Hide password" : "Show password"}
-                    >
-                      {authFormData.showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              },
-            }}
+            onChange={(val) =>
+              dispatch({ type: "passwordUpdated", payload: val })
+            }
+            showPassword={authFormData.showPassword}
+            onTogglePassword={() =>
+              dispatch({ type: "passwordVisibilityToggled" })
+            }
+            onFocus={() => {}}
+            passwordValid={true}
           />
 
           {warning && (
@@ -144,16 +107,25 @@ export default function LoginForm({
             </Box>
           )}
 
-          <SubmitButton type="submit" fullWidth variant="contained" disabled={isLoading}>
+          <SubmitButton
+            type="submit"
+            fullWidth
+            variant="contained"
+            disabled={isLoading}
+          >
             Sign In
           </SubmitButton>
         </Box>
 
-        {/* Cream/linen section: toggle link + OAuth + legal */}
         <Box sx={creamSectionSx}>
           <Typography variant="body2" sx={toggleTextSx}>
             Don't have an account?{" "}
-            <Link component="button" type="button" onClick={onToggleMode} sx={toggleLinkSx}>
+            <Link
+              component="button"
+              type="button"
+              onClick={onToggleMode}
+              sx={toggleLinkSx}
+            >
               Sign up
             </Link>
           </Typography>
@@ -164,7 +136,14 @@ export default function LoginForm({
             </Typography>
           </Divider>
 
-          <Button fullWidth variant="outlined" startIcon={<GoogleG />} onClick={onGoogleLogin} disabled={isLoading} sx={oauthButtonSx}>
+          <Button
+            fullWidth
+            variant="outlined"
+            startIcon={<GoogleG />}
+            onClick={onGoogleLogin}
+            disabled={isLoading}
+            sx={oauthButtonSx}
+          >
             Continue with Google
           </Button>
 
--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -2,24 +2,15 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
 import Divider from "@mui/material/Divider";
 import Link from "@mui/material/Link";
-import PersonIcon from "@mui/icons-material/Person";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import EmailIcon from "@mui/icons-material/Email";
-import LockIcon from "@mui/icons-material/Lock";
-import LockOpenIcon from "@mui/icons-material/LockOpen";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
-import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
 import GoogleG from "./GoogleG";
+import AuthTextInput from "./AuthTextInput";
 import {
   DARK_GREEN,
-  ICON_BROWN,
   MUTED_GRAY,
   ERROR_RED,
   WARNING_AMBER,
@@ -40,7 +31,6 @@ import {
   warningBoxSx,
   errorBoxSx,
   oauthButtonSx,
-  AuthTextField,
   SubmitButton,
 } from "../styles/authForms";
 import type {
@@ -86,7 +76,6 @@ export default function SignupForm({
 
   return (
     <>
-      {/* Logo */}
       <Box sx={logoBoxSx}>
         <RestaurantMenuIcon sx={{ fontSize: 28 }} />
         <Typography variant="h6" fontWeight={700} sx={logoTextSx}>
@@ -94,7 +83,6 @@ export default function SignupForm({
         </Typography>
       </Box>
 
-      {/* Title */}
       <Typography variant="h4" fontWeight={700} sx={pageTitleSx}>
         Create Your Account
       </Typography>
@@ -102,143 +90,61 @@ export default function SignupForm({
         Sign up to start saving &amp; sharing your favorite recipes.
       </Typography>
 
-      {/* Unified card: cork top + cream bottom */}
       <Box sx={outerCardSx}>
-        {/* Cork section: form fields + submit */}
         <Box component="form" onSubmit={onSubmit} noValidate sx={corkSectionSx}>
-          <AuthTextField
-            fullWidth
-            placeholder="First Name"
+          <AuthTextInput
+            variant="firstName"
             value={authFormData.firstName}
-            onChange={(e) =>
-              dispatch({ type: "firstNameUpdated", payload: e.target.value })
+            onChange={(val) =>
+              dispatch({ type: "firstNameUpdated", payload: val })
             }
-            required
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <PersonIcon sx={{ color: ICON_BROWN }} />
-                  </InputAdornment>
-                ),
-              },
-            }}
           />
 
-          <AuthTextField
-            fullWidth
-            placeholder="Last Name"
+          <AuthTextInput
+            variant="lastName"
             value={authFormData.lastName}
-            onChange={(e) =>
-              dispatch({ type: "lastNameUpdated", payload: e.target.value })
+            onChange={(val) =>
+              dispatch({ type: "lastNameUpdated", payload: val })
             }
-            required
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <PersonIcon sx={{ color: ICON_BROWN }} />
-                  </InputAdornment>
-                ),
-              },
-            }}
           />
 
-          <AuthTextField
-            fullWidth
-            placeholder="Username"
+          <AuthTextInput
+            variant="username"
             value={authFormData.username}
-            onChange={(e) =>
-              dispatch({ type: "usernameUpdated", payload: e.target.value })
+            onChange={(val) =>
+              dispatch({ type: "usernameUpdated", payload: val })
             }
-            required
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircleIcon sx={{ color: ICON_BROWN }} />
-                  </InputAdornment>
-                ),
-              },
-            }}
           />
 
-          <AuthTextField
-            fullWidth
-            type="email"
-            placeholder="Email"
+          <AuthTextInput
+            variant="email"
             value={authFormData.email}
-            onChange={(e) =>
-              dispatch({ type: "emailUpdated", payload: e.target.value })
-            }
-            required
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <EmailIcon sx={{ color: ICON_BROWN }} />
-                  </InputAdornment>
-                ),
-              },
-            }}
+            onChange={(val) => dispatch({ type: "emailUpdated", payload: val })}
           />
 
           <Box>
-            <AuthTextField
-              fullWidth
-              type={authFormData.showPassword ? "text" : "password"}
-              placeholder="Password"
+            <AuthTextInput
+              variant="password"
               value={authFormData.password}
-              onChange={(e) =>
-                dispatch({ type: "passwordUpdated", payload: e.target.value })
+              onChange={(val) =>
+                dispatch({ type: "passwordUpdated", payload: val })
+              }
+              showPassword={authFormData.showPassword}
+              onTogglePassword={() =>
+                dispatch({ type: "passwordVisibilityToggled" })
               }
               onFocus={() => setHasPasswordFocused(true)}
-              required
-              slotProps={{
-                input: {
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      {allRequirementsMet
-                        ? <LockIcon sx={{ color: ICON_BROWN }} />
-                        : <LockOpenIcon sx={{ color: ICON_BROWN }} />
-                      }
-                    </InputAdornment>
-                  ),
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        onClick={() =>
-                          dispatch({ type: "passwordVisibilityToggled" })
-                        }
-                        edge="end"
-                        size="small"
-                        sx={{ color: ICON_BROWN }}
-                        aria-label={
-                          authFormData.showPassword
-                            ? "Hide password"
-                            : "Show password"
-                        }
-                      >
-                        {authFormData.showPassword
-                          ? <VisibilityOffIcon />
-                          : <VisibilityIcon />
-                        }
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                },
-              }}
+              passwordValid={allRequirementsMet}
             />
-            {/* Clip container — overflow hidden collapses the space when hidden */}
             <Box sx={requirementsClipSx(showRequirements)}>
-              {/* Inner box slides up/down to sell the "from behind" illusion */}
               <Box sx={requirementsInnerSx(showRequirements)}>
                 {passwordRequirements.map(({ label, met }) => (
                   <Box key={label} sx={requirementRowSx}>
-                    {met
-                      ? <CheckIcon sx={{ fontSize: 14, color: DARK_GREEN }} />
-                      : <CloseIcon sx={{ fontSize: 14, color: ERROR_RED }} />
-                    }
+                    {met ? (
+                      <CheckIcon sx={{ fontSize: 14, color: DARK_GREEN }} />
+                    ) : (
+                      <CloseIcon sx={{ fontSize: 14, color: ERROR_RED }} />
+                    )}
                     <Typography
                       variant="caption"
                       sx={{ color: met ? DARK_GREEN : MUTED_GRAY }}
@@ -277,11 +183,15 @@ export default function SignupForm({
           </SubmitButton>
         </Box>
 
-        {/* Cream/linen section: toggle link + OAuth + legal */}
         <Box sx={creamSectionSx}>
           <Typography variant="body2" sx={toggleTextSx}>
             Already have an account?{" "}
-            <Link component="button" type="button" onClick={onToggleMode} sx={toggleLinkSx}>
+            <Link
+              component="button"
+              type="button"
+              onClick={onToggleMode}
+              sx={toggleLinkSx}
+            >
               Log in
             </Link>
           </Typography>
