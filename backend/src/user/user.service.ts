81dadbba22865b77890fe20c3dc66b6a4ef1680a
Wade Randel <waderandel6@gmail.com>
2026-03-11 12:29:05 -0600

    Added the requirements to the backend as stated in user-db-implementation-plan

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs'
+import { CreateUserDto } from '../dto/create-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
     async passwordValidation(password:string) {
         // Password Pattern: lowercase, UPPERCASE, digit, min length 8
-        // Do we want special characters aswell? 
-        // (?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]
-        const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
         return passPattern.test(password);
     }
 
@@ -21,40 +21,56 @@ export class UserService {
     async verifyPassword(password:string, hash:string) {
         // Compare password with hashed password, return true/false
         return await bcrypt.compare(password, hash);
-
     }
 
-
-    // Functions that need to be completed and potentially relocated
-    
-    async verifyUsername(username:string){
-        // Check username against DB to see if user already exists
-        // may be redundant but could be used during login or duing createUser()
-    }
-
-    async createUser(){
-        // if username in users:
-        //     notify that username already exists
-        // while true:
-        //     newPass = user input for password
-        //     isValid = validatePassword(newPass)
-        //     if isValid:
-        //         break
-        //     else:
-        //         notify user of invalid  password
-        
-        // create new user object for db storage
-        // newUser = User(new_username, hash_password(new_password), "user")
-
-        // save new user object to database
-        // saveUser(newUser)
-
-    }
-
-    async saveUser(){
-        // save new user to database
-        // for username, data in user_data.items():
-        //     save_user(username, data['password'], data['role'])
+    async validateEmail(email:string) {
+        const emailPattern = /^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/;
+        return emailPattern.test(email)
     }
     
+    // stuff from db imp plan
+
+    constructor(private readonly prisma: PrismaService) {}
+    
+    async findByUsername(username: string) {
+        return this.prisma.user.findUnique({ where: { username } });
+    }
+
+    async findById(id: string) {
+        return this.prisma.user.findUnique({ where: { id } });
+    }
+
+    async findByEmail(email: string) {
+        return this.prisma.user.findUnique({ where: { email } });
+    }
+
+    async registerUser(dto: CreateUserDto) {
+    // 1. Check email uniqueness
+    const emailTaken = await this.findByEmail(dto.email);
+    if (emailTaken) throw new ConflictException('Email already in use');
+
+    // 2. Check username uniqueness
+    const usernameTaken = await this.findByUsername(dto.username);
+    if (usernameTaken) throw new ConflictException('Username already taken');
+
+    // 3. Validate password shape
+    const validPassword = this.passwordValidation(dto.password);
+    if (!validPassword) throw new BadRequestException('Password does not meet requirements');
+
+    // 4. Hash password
+    const password_hash = await this.hashPassword(dto.password);
+
+    // 5. Create user in DB
+    const user = await this.prisma.user.create({
+        data: {
+            first_name: dto.firstName,
+            last_name: dto.lastName,
+            username: dto.username,
+            email: dto.email,
+            password_hash,
+        },
+    });
+
+    return user;
+    }
 }
